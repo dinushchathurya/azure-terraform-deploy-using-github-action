c994c203bf4600c125589de999728610fa624951
dinushchathurya <dinushchathurya21@gmail.com>
2022-04-30 21:01:14 +0530

    :construction_worker: Refactor Terraform Plan Job

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,9 +1,17 @@
 terraform {
-  cloud {
-    organization = "devninja"
-
-    workspaces {
-      name = "azure-terraform"
+    required_providers {
+        azurerm = {
+            source  = "hashicorp/azurerm"
+            version = "=3.4.0"
+        }
     }
-  }
+}
+
+provider "azurerm" {
+    features {}
+}
+
+resource "azurerm_resource_group" "platform_rg" {
+    name = var.resource_group_name
+    location = var.location
 }
