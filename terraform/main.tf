09e23b23064840eabe61b81de77a561384a005c6
dinushchathurya <dinushchathurya21@gmail.com>
2022-05-01 08:48:18 +0530

    :wrench: Crete new terraform configrations

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,16 +1,18 @@
 terraform {
   backend "azurerm" {
-    resource_group_name  = "TerraformDemo"
-    storage_account_name = "terraformcodeops"
+    resource_group_name  = "GithubAction"
+    storage_account_name = "githubactiondemo"
     container_name       = "tfstatefile"
     key                  = "dev.terraform.tfstate"
   }
 }
+
 module "RG" {
   source   = "./modules/RG" #A
   rgname   = var.rgname     #B
   location = var.location
 }
+
 module "SA" {
   source   = "./modules/StorageAccount"
   sname    = var.sname
